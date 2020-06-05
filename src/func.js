ef190561defa91c3019442ed5b83ade9ca7779e0
Liena Vu <liena_vu@epam.com>
2020-06-05 12:07:53 +0300

    functions

---
+++ src/func.js
@@ -0,0 +1,17 @@
+const getSum = (str1, str2) => {
+  // add your implementation below
+  return;
+};
+
+const getQuantityPostsByAuthor = (listOfPosts, authorName) => {
+  // add your implementation below
+  return;
+};
+
+const tickets=(people)=> {
+  // add your implementation below
+  return;
+};
+
+
+module.exports = {getSum, getQuantityPostsByAuthor, tickets};
