1f3723250cbbe115da9d2a94bea5dace7be8a166
Mariia Korzun <3151020m@gmail.com>
2020-11-26 15:16:08 +0200

    Function second HW is done.

--- src/func.js
+++ src/func.js
@@ -1,17 +1,51 @@
 const getSum = (str1, str2) => {
-  // add your implementation below
-  return;
-};
+  if (isNaN(str1) || Array.isArray(str1) || isNaN(str2) || Array.isArray(str2)) { return false }
+  str1 = str1 === '' ? 0 : parseFloat(str1)
+  str2 = str2 === '' ? 0 : parseFloat(str2)
+  return `${str1 + str2}`
+}
 
 const getQuantityPostsByAuthor = (listOfPosts, authorName) => {
-  // add your implementation below
-  return;
-};
+  let posts = listOfPosts.filter(post => post.author === authorName).length
+  let comment = listOfPosts.reduce((sum, post) => {
+    return sum += post.comments === undefined ? 0 :
+      post.comments.filter(comment => comment.author === authorName).length
+  }, 0)
 
-const tickets=(people)=> {
-  // add your implementation below
-  return;
-};
+  return `Post:${posts},comments:${comment}`
+}
 
+const tickets = (people) => {
+  let bill25 = 0, bill50 = 0
 
-module.exports = {getSum, getQuantityPostsByAuthor, tickets};
+  for (let i = 0; i < people.length; i++) {
+    switch (people[i]) {
+      case 25: bill25++
+        break
+
+      case 50: {
+        if (bill25 < 1) { return 'NO' }
+        bill25--
+        bill50++
+        break
+      }
+      case 100: {
+        if (bill50 > 0 && bill25 > 0) {
+          bill50--
+          bill25--
+        }
+        else if (bill25 > 2) { bill25 -= 3 }
+        else {
+          return 'NO'
+        }
+        break
+      }
+      default: {
+        return 'NO'
+      }
+    }
+  }
+  return 'YES'
+}
+
+module.exports = { getSum, getQuantityPostsByAuthor, tickets };
